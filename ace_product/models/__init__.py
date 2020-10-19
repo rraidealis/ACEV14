46084768a16d7657db99da75e04942ba57843568
DWA <dwa@idealisconsulting.com>
2020-10-19 02:37:35 +0200

    [FIX] Compatibility v14

--- ace_product/models/__init__.py
+++ ace_product/models/__init__.py
@@ -1,14 +1,14 @@
 # -*- coding: utf-8 -*-
 
-from . import family_code
-from . import technical_description
-from . import formula_code
-from . import color_code
-from . import commercial_name
-from . import packing_norm
-from . import pallet_type
-from . import product_template
-# not necessary in V14
-# from . import uom_category
-from . import mrp_bom
-from . import product_category
+# from . import family_code
+# from . import technical_description
+# from . import formula_code
+# from . import color_code
+# from . import commercial_name
+# from . import packing_norm
+# from . import pallet_type
+# from . import product_template
+## not necessary in V14
+## from . import uom_category
+# from . import mrp_bom
+# from . import product_category
