cc0029946a9ede13838b18d32579235448e9afbc
DWA <dwa@idealisconsulting.com>
2020-12-03 19:48:04 +0100

    [I18N] ACE-5 -> translations

--- ace_uom/models/product_template.py
+++ ace_uom/models/product_template.py
@@ -41,17 +41,37 @@ class ProductTemplate(models.Model):
                     product.update({'show_banner_missing_uom': True, 'sale_ok': False, 'purchase_ok': False})
 
     def generate_uoms_action(self):
+        """
+        Generate uoms if product category is set to handle uom templates.
+        Note: This action may be called several times
+        1. Check if there is an uom category template set on product category then,
+        2. Retrieve uom category from uom_id
+        2a. If uom_id category has been created from a template and this template is the same
+            as the one set on product category then keep reference and archives the other ones
+        2b. Else try to create a new uom category with information from uom category template. If category name is
+            not valid or is already in use, then raise an error
+        3. Look for duplicates or non-existence of reference, default uom and default purchase uom in uom templates
+        4. If a reference has been kept, update it, else create a new one from template
+        5. Create other uoms. This should be done only if there is one reference unit.
+        6. Updates uom_id and uom_po_id on product with recently created uoms
+        """
         self.ensure_one()
         category_template = self.categ_id.uom_category_template_id
+        # 1. Check if there is an uom category template set on product category
         if category_template:
+            # 2. Retrieve uom category from uom_id
             default_uom_category = self.uom_id.category_id \
                 if self.uom_id.category_id.category_template_id \
                    and self.uom_id.category_id.category_template_id == category_template \
                 else False
+            # 2a.   If uom_id category has been created from a template and this template is the same
+            #       as the one set on product category then keep reference and archives the other ones
             if default_uom_category:
                 uoms = self.env['uom.uom'].search([('category_id', '=', default_uom_category.id), ('active', '=', True)])
                 reference_uom = uoms.filtered(lambda uom: uom.uom_type == 'reference')
                 (uoms - reference_uom).write({'active': False})
+            # 2b.   Else try to create a new uom category with information from uom category template. If category name is
+            #       not valid or is already in use, then raise an error
             else:
                 category_name = getattr(self, category_template.name_from)
                 if not category_name:
@@ -61,6 +81,7 @@ class ProductTemplate(models.Model):
                     raise ValidationError(_('There is already an UoM category with this name ({}).').format(category_name))
                 default_uom_category = self.env['uom.category'].create({'name': category_name, 'category_template_id': category_template.id})
                 reference_uom = self.env['uom.uom']
+            # 3. Look for duplicates or non-existence of reference, default uom and default purchase uom in uom templates
             reference_uom_template = category_template.uom_template_ids.filtered(lambda x: x.uom_type == 'reference')
             default_uom_template = category_template.uom_template_ids.filtered(lambda x: x.default_uom)
             default_uom_po_template = category_template.uom_template_ids.filtered(lambda x: x.default_purchase_uom)
@@ -70,6 +91,7 @@ class ProductTemplate(models.Model):
                 raise ValidationError(_('Category template {} should have one "default" uom template (found {}).').format(category_template, len(default_uom_template)))
             if len(default_uom_po_template) != 1:
                 raise ValidationError(_('Category template {} should have one "default purchase" uom template (found {}).').format(category_template, len(default_uom_po_template)))
+            # 4. If a reference has been kept, update it, else create a new one from template
             if not reference_uom:
                 reference_uom = self.env['uom.uom'].create(self._prepare_uom_values(reference_uom_template, default_uom_category))
             else:
@@ -78,13 +100,19 @@ class ProductTemplate(models.Model):
                     'category_id': default_uom_category.id,
                     'rounding': reference_uom_template.rounding,
                     'uom_template_id': reference_uom_template.id})
+            # 5. Create other uoms. This should be done only if there is one reference unit
             for uom_template in (category_template.uom_template_ids - reference_uom_template):
                 self.env['uom.uom'].create(self._prepare_uom_values(uom_template, default_uom_category))
+            # 6. Updates uom_id and uom_po_id on product with recently created uoms
             default_uom = self.env['uom.uom'].search([('uom_template_id', '=', default_uom_template.id), ('active', '=', True)])
             default_uom_po = self.env['uom.uom'].search([('uom_template_id', '=', default_uom_po_template.id), ('active', '=', True)])
             self.write({'uom_id': default_uom.id, 'uom_po_id': default_uom_po.id})
 
     def _prepare_uom_values(self, template, category):
+        """
+        Prepare uom values from uom template and uom category
+        If factor is not different than 0.0, raise an error
+        """
         if template.factor_from:
             factor = getattr(self, template.factor_from)
         else:
