b91d8ab2bb484be1df1f56eb78ac9b13a9ffa56a
DWA <dwa@idealisconsulting.com>
2021-01-20 09:41:53 +0100

    refresh dev

--- ace_bom/wizard/import_recipe.py
+++ ace_bom/wizard/import_recipe.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+# Part of Idealis Consulting. See LICENSE file for full copyright and licensing details.
 from odoo import fields, models, _
 from odoo.exceptions import UserError
 
